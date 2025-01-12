654ee0ad5c4634abc6e430fe86e17bb1bc832511
mnb_h <mnb_h@Abuhasan>
2025-01-12 07:06:11 +0300

    add print check

--- JavaFXTest/src/hellofx/Main.java
+++ JavaFXTest/src/hellofx/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+    	System.out.println("check");
         Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 900, 900));
