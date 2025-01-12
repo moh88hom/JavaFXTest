552ffd77829a2cf477d37d4e1e6d1733cf39ef86
moh88hom <94586885+moh88hom@users.noreply.github.com>
2025-01-12 05:54:34 +0300

    Update Controller.java

--- JavaFXTest/src/hellofx/Controller.java
+++ JavaFXTest/src/hellofx/Controller.java
@@ -10,6 +10,7 @@ public class Controller {
 
     public void initialize() 
     {
+        //comment
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
